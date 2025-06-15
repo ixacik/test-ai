fe3d1741e35026704ba5b82ac23aa59f85f73b2f
ixacik <patriklevak@gmail.com>
2025-06-15 13:15:47 +0200

    anal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Analytics />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
